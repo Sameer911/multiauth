22aadf7d58887368908d7d0996330081ee03bdd0
qadir@vfairs.com <qadir@vfairs.com>
2022-10-21 16:12:13 +0500

    user dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,6 @@
                   <div class="card-content">
                     <h5 class="font-15">Cash In Hand</h5>
                     <h2 class="mb-3 font-18">{{$Cashinhand}}  PKR</h2>
-                    <p class="mb-0"><span class="col-green">10%</span> Increase</p>
                   </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -38,9 +37,8 @@
               <div class="row ">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                   <div class="card-content">
-                    <h5 class="font-15"> Customers</h5>
-                    <h2 class="mb-3 font-18">1,287</h2>
-                    <p class="mb-0"><span class="col-orange">09%</span> Decrease</p>
+                    <h5 class="font-15"> Pending Order</h5>
+                    <h2 class="mb-3 font-18">{{$pendingOrders}}</h2>
                   </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -60,37 +58,13 @@
               <div class="row ">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                   <div class="card-content">
-                    <h5 class="font-15">New Project</h5>
-                    <h2 class="mb-3 font-18">128</h2>
-                    <p class="mb-0"><span class="col-green">18%</span>
-                      Increase</p>
-                  </div>
+                    <h5 class="font-15">Paid Orders</h5>
+                    <h2 class="mb-3 font-18">{{$paidOrders}}</h2>
+                    </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                   <div class="banner-img">
                     <img src="assets/img/banner/3.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <div class="card">
-          <div class="card-statistic-4">
-            <div class="align-items-center justify-content-between">
-              <div class="row ">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
-                  <div class="card-content">
-                    <h5 class="font-15">Revenue</h5>
-                    <h2 class="mb-3 font-18">$48,697</h2>
-                    <p class="mb-0"><span class="col-green">42%</span> Increase</p>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
-                  <div class="banner-img">
-                    <img src="assets/img/banner/4.png" alt="">
                   </div>
                 </div>
               </div>
